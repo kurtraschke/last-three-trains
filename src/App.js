db4f5bb534465ef4a17e1e40d56f567c1da75eb3
Kurt Raschke <kurt@kurtraschke.com>
2022-09-27 20:16:49 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "@patternfly/react-core/dist/styles/base.css";
 import "./App.css";
@@ -12,64 +12,82 @@ import {
     PageSection,
     Toolbar,
     ToolbarContent,
-    ToolbarItem, EmptyState, EmptyStateIcon, Title, EmptyStateBody, FlexItem, Flex,
+    EmptyState,
+    EmptyStateIcon,
+    Title,
+    EmptyStateBody,
+    FlexItem,
+    Flex,
 } from "@patternfly/react-core";
 import CubesIcon from "@patternfly/react-icons/dist/esm/icons/cubes-icon";
+
 import StationSelector from "./StationSelector";
 import RunningTimes from "./RunningTimes";
 
 
 
 const App = () => {
-    const header = <Masthead>
-        <MastheadMain>
-            <MastheadBrand href="/" target="_blank">
-                Last Three Trains
-            </MastheadBrand>
-        </MastheadMain>
-        <MastheadContent>
-            <Toolbar id="fill-toolbar">
-                <ToolbarContent>
-                </ToolbarContent>
-            </Toolbar>
-        </MastheadContent>
-    </Masthead>;
+    const header = (
+        <Masthead>
+            <MastheadMain>
+                <MastheadBrand href="/" target="_blank">
+                    Last Three Trains
+                </MastheadBrand>
+            </MastheadMain>
+            <MastheadContent>
+                <Toolbar id="fill-toolbar">
+                    <ToolbarContent></ToolbarContent>
+                </Toolbar>
+            </MastheadContent>
+        </Masthead>
+    );
 
     const [selectedEntry, setSelectedEntry] = useState();
     const [selectedExit, setSelectedExit] = useState();
 
-    return <Page header={header}>
-        <PageSection>
-            <Flex direction={{ default: 'column', lg: 'row' }} grow={{ default: 'grow' }}>
-                <FlexItem>
-                    <StationSelector selected={selectedEntry} setSelected={setSelectedEntry}
-                                     placeholderText={"Select entry station"}/>
-                </FlexItem>
-                <FlexItem>
-                    <StationSelector selected={selectedExit} setSelected={setSelectedExit}
-                                     placeholderText={"Select exit station"}/>
-                </FlexItem>
-            </Flex>
-        </PageSection>
-        <PageSection>
-            {
-                (!!selectedEntry && !!selectedExit) ? (
-                    <RunningTimes entryStation={selectedEntry} exitStation={selectedExit} />
-
+    return (
+        <Page header={header}>
+            <PageSection>
+                <Flex
+                    direction={{ default: "column", lg: "row" }}
+                    grow={{ default: "grow" }}>
+                    <FlexItem>
+                        <StationSelector
+                            selected={selectedEntry}
+                            setSelected={setSelectedEntry}
+                            placeholderText={"Select entry station"}
+                        />
+                    </FlexItem>
+                    <FlexItem>
+                        <StationSelector
+                            selected={selectedExit}
+                            setSelected={setSelectedExit}
+                            placeholderText={"Select exit station"}
+                        />
+                    </FlexItem>
+                </Flex>
+            </PageSection>
+            <PageSection>
+                {!!selectedEntry && !!selectedExit ? (
+                    <RunningTimes
+                        entryStation={selectedEntry}
+                        exitStation={selectedExit}
+                    />
                 ) : (
                     <EmptyState>
-                        <EmptyStateIcon icon={CubesIcon}/>
+                        <EmptyStateIcon icon={CubesIcon} />
                         <Title headingLevel="h4" size="lg">
                             Select stations
                         </Title>
                         <EmptyStateBody>
-                            Select an entry and exit station to view running times.
+                            Select an entry and exit station to view running
+                            times.
                         </EmptyStateBody>
                     </EmptyState>
-                )
-            }
-        </PageSection>
-    </Page>;
+                )}
+            </PageSection>
+        </Page>
+    );
 };
 
 export default App;
--- src/RunningTimes.js
+++ src/RunningTimes.js
@@ -1,66 +1,114 @@
-import React, {useState} from "react";
-import {useQuery} from "@tanstack/react-query";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import {TableComposable, Caption, Thead, Tr, Th, Tbody, Td} from "@patternfly/react-table";
-import {DateTime} from "luxon";
-import {EmptyState, EmptyStateIcon, Spinner, Title} from "@patternfly/react-core";
+import {
+    TableComposable,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+} from "@patternfly/react-table";
+import { DateTime } from "luxon";
+import {
+    EmptyState,
+    EmptyStateIcon,
+    Spinner,
+    Title,
+} from "@patternfly/react-core";
+import urlcat from "urlcat";
 
 import ExclamationCircleIcon from "@patternfly/react-icons/dist/esm/icons/exclamation-circle-icon";
 
+const API_BASE =
+    process.env.NODE_ENV === "development" && !!process.env.REACT_APP_API_BASE
+        ? process.env.REACT_APP_API_BASE
+        : "https://api.choochoo.systems/";
 
-function RunningTimes({entryStation, exitStation}) {
-    const {isLoading, error, data} = useQuery(["runningTimes", entryStation, exitStation],
-        ({signal}) =>
-            axios.get("/rpc/running_times",
-                {
+function RunningTimes({ entryStation, exitStation }) {
+    const { isLoading, error, data } = useQuery(
+        ["runningTimes", entryStation, exitStation],
+        ({ signal }) =>
+            axios
+                .get(urlcat(API_BASE, "/rpc/running_times"), {
                     params: {
-                        "entry_station": entryStation,
-                        "exit_station": exitStation,
-                        "order": "exit_time.desc"
+                        entry_station: entryStation,
+                        exit_station: exitStation,
+                        order: "exit_time.desc",
                     },
                     signal,
                 })
-                .then((res) => res.data),
-    )
+                .then((res) => res.data)
+    );
 
-    if (isLoading) return <EmptyState>
-        <EmptyStateIcon variant="container" component={Spinner} />
-        <Title size="lg" headingLevel="h4">
-            Loading
-        </Title>
-    </EmptyState>;
+    if (isLoading) {
+        return (
+            <EmptyState>
+                <EmptyStateIcon variant="container" component={Spinner} />
+                <Title size="lg" headingLevel="h4">
+                    Loading
+                </Title>
+            </EmptyState>
+        );
+    }
 
-    if (error) return <EmptyState>
-        <EmptyStateIcon variant="container" component={ExclamationCircleIcon} />
-        <Title size="lg" headingLevel="h4">
-            Error: {error.message}
-        </Title>
-    </EmptyState>;
+    if (error) {
+        return (
+            <EmptyState>
+                <EmptyStateIcon
+                    variant="container"
+                    component={ExclamationCircleIcon}
+                />
+                <Title size="lg" headingLevel="h4">
+                    Error: {error.message}
+                </Title>
+            </EmptyState>
+        );
+    }
 
-    return <TableComposable>
-        <Thead>
-            <Tr>
-                <Th>Train</Th>
-                <Th>Entry</Th>
-                <Th>Exit</Th>
-                <Th>Running Time</Th>
-            </Tr>
-        </Thead>
-        <Tbody>
-            {data.map((train, index) => {
-                const entryTime = DateTime.fromISO(train['entry_time']);
-                const exitTime = DateTime.fromISO(train['exit_time']);
-
-
-                return <Tr key={index}>
-                    <Td dataLabel={"Train"}>{train["train_id"]}</Td>
-                    <Td dataLabel={"Entry"}>{entryTime.setZone("US/Eastern").toLocaleString(DateTime.TIME_WITH_SECONDS)}</Td>
-                    <Td dataLabel={"Exit"}>{exitTime.setZone("US/Eastern").toLocaleString(DateTime.TIME_WITH_SECONDS)}</Td>
-                    <Td dataLabel={"Running Time"}>{exitTime.diff(entryTime, ['hours', 'minutes', 'seconds']).toHuman({ unitDisplay: "short" })}</Td>
+    return (
+        <TableComposable>
+            <Thead>
+                <Tr>
+                    <Th>Train</Th>
+                    <Th>Entry</Th>
+                    <Th>Exit</Th>
+                    <Th>Running Time</Th>
                 </Tr>
-            })}
-        </Tbody>
-    </TableComposable>
+            </Thead>
+            <Tbody>
+                {data.map((train, index) => {
+                    const entryTime = DateTime.fromISO(train["entry_time"]);
+                    const exitTime = DateTime.fromISO(train["exit_time"]);
+
+                    return (
+                        <Tr key={index}>
+                            <Td dataLabel={"Train"}>{train["train_id"]}</Td>
+                            <Td dataLabel={"Entry"}>
+                                {entryTime
+                                    .setZone("US/Eastern")
+                                    .toLocaleString(DateTime.TIME_WITH_SECONDS)}
+                            </Td>
+                            <Td dataLabel={"Exit"}>
+                                {exitTime
+                                    .setZone("US/Eastern")
+                                    .toLocaleString(DateTime.TIME_WITH_SECONDS)}
+                            </Td>
+                            <Td dataLabel={"Running Time"}>
+                                {exitTime
+                                    .diff(entryTime, [
+                                        "hours",
+                                        "minutes",
+                                        "seconds",
+                                    ])
+                                    .toHuman({ unitDisplay: "short" })}
+                            </Td>
+                        </Tr>
+                    );
+                })}
+            </Tbody>
+        </TableComposable>
+    );
 }
 
-export default RunningTimes
+export default RunningTimes;
